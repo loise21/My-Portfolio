54b0c28c9acfd97fb0a352513088551896f69093
loise21 <138427716+loise21@users.noreply.github.com>
2024-05-21 22:31:35 +0800

    Add files via upload

---
+++ sariego.js
@@ -0,0 +1,31 @@
+// script.js
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    // You can add your form submission logic here, like sending the data via AJAX
+    alert('Form submitted!');
+});
+
+
+let index = 0,
+    interval = 1000;
+
+const rand = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const animate = star => {
+  star.style.setProperty("--star-left", ${rand(-10, 100)}%);
+  star.style.setProperty("--star-top", ${rand(-40, 80)}%);
+
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+}
+
+for(const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
+    
+    setInterval(() => animate(star), 1000);
+  }, index++ * (interval / 3))
+}
